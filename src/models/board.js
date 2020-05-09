43d925d3f0a1fa8f722d92e9ceda6374575fa9c8
romefar <vlad.romanovr@gmail.com>
2020-05-09 21:33:03 +0300

    added board schema

---
+++ src/models/board.js
@@ -0,0 +1,67 @@
+const { Schema, model } = require('mongoose')
+const User = require('./user')
+
+const cardSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Card name is required.']
+  },
+  description: {
+    type: String,
+    required: [true, 'Card description is required.']
+  },
+  estimate: {
+    type: String,
+    required: [true, 'Estimated time is required.']
+  },
+  status: {
+    type: String,
+    enum: ['Todo', 'In progress', 'In review', 'Done'],
+    default: 'Todo'
+  },
+  dueDate: {
+    type: Date,
+    required: [true, 'Due date is required.']
+  },
+  labels: [{
+    type: String,
+    trim: true
+  }],
+  user: {
+    type: Schema.Types.ObjectId,
+    required: [true, 'User is required.'],
+    ref: 'Users'
+  }
+})
+
+const boardSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    default: 'white'
+  },
+  description: {
+    type: String
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  cards: [cardSchema]
+}, { timestamps: true })
+
+boardSchema.pre('save', async function (next) {
+  const board = this
+  const { role } = await User.findById(board.owner)
+  if (role !== 'admin') throw new Error('You don\'t have permissions to create a board.')
+
+  next()
+})
+
+const Board = model('Board', boardSchema)
+
+module.exports = Board
