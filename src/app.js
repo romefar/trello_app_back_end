8beac44f473f453493c92bee57370c24b8a68dd8
romefar <vlad.romanovr@gmail.com>
2020-05-12 19:43:21 +0300

    Updated entry point, added connection handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const chalk = require('chalk')
 const helmet = require('helmet')
+const logger = require('./config/winston')
+const connect = require('./db/mongoose')
 require('dotenv/config')
-require('./db/mongoose')
 
-const usersRoute = require('./routes/users')
+const genericRoute = require('./routes/generic')
 const authRoute = require('./routes/auth')
 const boardsRoute = require('./routes/boards')
 const cardsRoute = require('./routes/cards')
@@ -16,12 +16,19 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(helmet())
 app.use('/', authRoute)
-app.use('/users', usersRoute)
+app.use('/', genericRoute)
 
 app.use('/profile/boards', boardsRoute)
 app.use('/profile/boards', cardsRoute)
 app.use('/profile', profileRoute)
 
-app.listen(port, () => {
-  console.log(chalk.green(`\n - Server is running on port ${port}`))
-})
+connect()
+  .then(() => {
+    logger.info('Succesfully connected to the database.')
+    app.listen(port, () => {
+      logger.info(`Server is running on port ${port}.`)
+    })
+  })
+  .catch(err => {
+    logger.error(`Couldn't connect to the database. - ${err.message}`)
+  })
