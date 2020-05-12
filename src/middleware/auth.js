01dcc4520a9825c6f8b10eea07eeb21fa9eb09e7
romefar <vlad.romanovr@gmail.com>
2020-05-12 22:11:19 +0300

    updated auth function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,6 +5,7 @@ require('dotenv/config')
 
 const auth = async (req, res, next) => {
   try {
+    if (!req.header('Authorization')) throw new Error('You have to provide credentials.')
     const token = req.header('Authorization').replace('Bearer ', '')
     const decoded = jwt.verify(token, process.env.SECRET_KEY)
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
