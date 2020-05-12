caefadaae0434cf7a7f794ed6e7548abf01dc1cc
romefar <vlad.romanovr@gmail.com>
2020-05-12 19:39:34 +0300

    Updated connection function

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose')
-const chalk = require('chalk')
 require('dotenv/config')
 
-mongoose.connect(`mongodb://${process.env.DB_DEV_HOST}:${process.env.DB_DEV_DB_PORT}/${process.env.DB_DEV_NAME}`, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false
-})
-  .then(() => {
-    console.log(chalk.green(' -- Succesfully connected to the database.'))
+module.exports = () => {
+  const connection = mongoose.connect(`mongodb://${process.env.DB_DEV_HOST}:${process.env.DB_DEV_DB_PORT}/${process.env.DB_DEV_NAME}`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
   })
-  .catch(err => {
-    console.log(chalk.red('An error was occured.'))
-    console.log(err)
-  })
-
-mongoose.set('useCreateIndex', true)
+  mongoose.set('useCreateIndex', true)
+  return connection
+}
